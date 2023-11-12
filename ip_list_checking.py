e840df9d4bcceb32c320bd7977ead2c3a5e4563b
Md Al Amin <mdalaminbijoy3@gmail.com>
2023-11-12 19:46:29 +0600

    [INCOMPLETE] using both dnspython and aiodns to check ip list to see which performs better

--- ip_list_checking.py
+++ ip_list_checking.py
@@ -2,52 +2,27 @@ import asyncio
 import json
 from time import perf_counter
 from backend import check_status, results
+# from backend_aiodns import check_status as aiodns_check_status
 
 # SOLVED.
 # Throws Error.
 # OSError: [Errno 24] Too many open files: '/etc/resolv.conf'
-def get_host_addresses(cidr = '202.83.124.0/26'):
+def get_host_addresses(cidr = '202.83.124.0/25'):
     import ipaddress
     subnet = ipaddress.IPv4Network(cidr, strict=False)
     hosts = list(map(lambda ip: ip.__str__(), subnet.hosts()))
     return hosts 
 
-ip_addresses = get_host_addresses()
+ip_addresses1 = get_host_addresses()
+ip_addresses2 = get_host_addresses(cidr='202.83.124.128/24')
 
-# ip_addresses = [
-#     '103.251.167.4',
-#     '103.251.167.5',
-#     '103.251.167.6',
-#     '103.251.167.7',
-#     '103.251.167.8',
-#     '103.251.167.9',
-#     '103.251.167.10',
-#     '103.251.167.11',
-#     '103.251.167.12',
-#     '103.251.167.13',
-#     '103.251.167.14',
-#     '103.251.167.15',
-#     '103.251.167.16',
-#     '103.251.167.17',
-#     '103.251.167.18',
-#     '103.251.167.19',
-#     '103.251.167.20',
-#     '103.251.167.21',
-#     '103.251.167.22',
-#     '103.251.167.23',
-#     '103.251.167.24',
-#     '103.251.167.25',
-#     '103.251.167.26',
-#     '103.251.167.27',
-#     '103.251.167.28',
-#     '103.251.167.29',
-#     '103.251.167.30',
-# ]
+
 
 async def get_statuses(ip_list):
     task_list = []
     for ip in ip_list:
         task = asyncio.create_task(check_status(ip))
+        # task = asyncio.create_task(aiodns_check_status(ip))
         task_list.append(task)
     statuses_list  = await asyncio.gather(*task_list)
     ip_list_status_result = dict()
@@ -57,8 +32,9 @@ async def get_statuses(ip_list):
 
 
 
+import multiprocessing
 start = perf_counter()
-print(asyncio.run(get_statuses(ip_addresses)))
+print(asyncio.run(get_statuses(ip_addresses2)))
 end = perf_counter()
 
 print('############### TIME TAKES ############## :: ', end - start)
